fbf3acdc78505b7860d0ec1a3580078235c544be
justin <justin@justin-OptiPlex-3010.(none)>
2017-05-03 16:10:38 +0800

    Add BPI-M2M customize moduel

---
+++ android/device/softwinner/bpi-m2m/gps/gps.mk
@@ -0,0 +1,5 @@
+PRODUCT_COPY_FILES += \
+   device/softwinner/bpi-m2m/gps/gps.default.so:system/lib/hw/gps.default.so \
+   device/softwinner/bpi-m2m/gps/glgps:system/bin/glgps \
+   device/softwinner/bpi-m2m/gps/gpslogd:system/bin/gpslogd \
+   device/softwinner/bpi-m2m/gps/gpsconfig.xml:system/etc/gps/gpsconfig.xml
