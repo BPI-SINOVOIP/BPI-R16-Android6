b21318fbc3462857254e47c7e9e5d440e9893003
Justin <Justin>
2019-07-25 10:47:13 +0800

    Add BPI-M2M-LCD7 Module

---
+++ android/device/softwinner/bpi-m2m-lcd7/gps/gps.mk
@@ -0,0 +1,5 @@
+PRODUCT_COPY_FILES += \
+   device/softwinner/bpi-m2m-lcd7/gps/gps.default.so:system/lib/hw/gps.default.so \
+   device/softwinner/bpi-m2m-lcd7/gps/glgps:system/bin/glgps \
+   device/softwinner/bpi-m2m-lcd7/gps/gpslogd:system/bin/gpslogd \
+   device/softwinner/bpi-m2m-lcd7/gps/gpsconfig.xml:system/etc/gps/gpsconfig.xml
